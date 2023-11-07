9710a53e3f9bb67fc3d96e99e069cbca97611bf5
Evrim Persembe <evrimfeyyaz@gmail.com>
2023-11-07 18:23:40 +0300

    Initial commit

--- app.config.js
+++ app.config.js
@@ -1,6 +1,9 @@
-{
+const name = process.env.BUILD_TYPE === 'DEV_STAGE' ? 'Dev Stage Test' : 'Dev Prod Test';
+const identifier = process.env.BUILD_TYPE === 'DEV_STAGE' ? 'com.example.devstage' : 'com.example.devprod';
+
+export default {
   "expo": {
-    "name": "multiple-dev-clients-test",
+    "name": name,
     "slug": "multiple-dev-clients-test",
     "version": "1.0.0",
     "orientation": "portrait",
@@ -15,16 +18,23 @@
       "**/*"
     ],
     "ios": {
-      "supportsTablet": true
+      "supportsTablet": true,
+      "bundleIdentifier": identifier
     },
     "android": {
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
-      }
+      },
+      "package": identifier
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    "extra": {
+      "eas": {
+        "projectId": "ed11fc31-a829-4e9e-a8b2-b898608450a7"
+      }
     }
   }
 }
